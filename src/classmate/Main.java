0b07e1bf6b696915c3cfaf3e2639ce230fef5440
DESKTOP-AQHOO7B\Admin <benfcquang@gmail.com>
2023-03-02 17:06:28 +0700

    update arraylist

--- src/classmate/Main.java
+++ src/classmate/Main.java
@@ -1,12 +1,19 @@
 package classmate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main (String[] args){
-        Student stu = new Student("Trung",23,2000);
-        Student stu2 = new Student("Son");
-        StudentB stu3 = new StudentB("Son",21,1999);
 
-        stu3.getnewInfo();
+        List<Student> stulist = new ArrayList<>();
+        Student stu1 = new Student("Trung",22,2000);
+        stulist.add(stu1);
+        stu1 = new Student("Son",23,2000);
+        System.out.println(stu1);
+
+
+
 
     }
 }
