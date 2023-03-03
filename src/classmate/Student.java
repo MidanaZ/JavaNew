b3e137de0cad9cabbaf451052dc7ff4af41b6fbc
DESKTOP-AQHOO7B\Admin <benfcquang@gmail.com>
2023-03-03 17:18:57 +0700

    update nthu71

--- src/classmate/Student.java
+++ src/classmate/Student.java
@@ -42,4 +42,9 @@ public class Student {
         System.out.println(""+age);
         System.out.println(""+year);
     }
+
+    @Override
+    public String toString() {
+        return getName()+" "+getAge()+" "+getYear();
+    }
 }
